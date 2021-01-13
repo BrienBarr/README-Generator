f1795ce7268b75270feec397d6d04d01bd820fb8
BrienB <barrdom@hotmail.com>
2021-01-12 23:44:24 -0500

    added first question to inquirer prompt

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'projectName'
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(filename, data);
 }
 
 // function to initialize program
 function init() {
-
+    // console.log(questions);
+    inquirer
+    .prompt(
+        questions
+    )
+    .then((response) => {
+        console.log(`${response.projectName}`);
+    })
+    // .then(async (response) =>
+    //     fileName = "README.md",
+    //     data = `
+    //         # ${response.projectName}
+    //     `,
+    //     await (writeToFile(fileName, data)),
+    // )
 }
 
 // function call to initialize program
