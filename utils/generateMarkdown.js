c8e0e0102ec2f201d66b5a240d70aa89877dfd6c
BrienB <barrdom@hotmail.com>
2021-01-16 23:31:53 -0500

    updated TOC syntax in markdown file data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -48,13 +48,13 @@ ${licenseBadge}
 
 ## Table of Contents
 
-1. [Description](##Description)
-2. [Installation](##Installation)
-3. [Usage](##Usage)
-4. [Contributing](##Contributing)  
-5. [Tests](##Tests)
-6. [License](##License)
-7. [Questions](##Questions)
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Contributing](#Contributing)  
+5. [Tests](#Tests)
+6. [License](#License)
+7. [Questions](#Questions)
 <br/>
 <br/>
 
